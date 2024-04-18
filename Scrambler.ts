69f14a13b13557048eee1d57b8444e40d0186d74
Ian Frye <ifrye@theresource.com>
2024-04-18 07:45:06 -0400

    added comments and restrucutred class

--- Scrambler.ts
+++ Scrambler.ts
@@ -1,3 +1,4 @@
+// Purpose: Generate scrambles for various WCA puzzles
 enum PuzzleType {
   '2x2' = '2x2',
   '3x3' = '3x3',
@@ -13,7 +14,8 @@ enum PuzzleType {
 }
 
 class ScrambleGenerator {
-  private active: Set<string> = new Set<string>();
+  private active: Set<string> = new Set<string>(); // Set to keep track of active moves
+  // Map to store opposite moves for intersection removal
   private opposite: { [key: string]: string } = {
     U: 'D',
     D: 'U',
@@ -23,43 +25,19 @@ class ScrambleGenerator {
     B: 'F',
   };
 
+  // Generate a random number within a specified limit
   private randomNum(limit: number): number {
     return Math.floor(Math.random() * limit);
   }
 
+  // Remove intersecting moves and their opposites from the active set
   private removeIntersections(curr: string): void {
     this.active.forEach((move) => {
       if (move !== curr && move !== this.opposite[curr]) this.active.delete(move);
     });
   }
 
-  private standardScramble(size: string, length: number): string[] {
-    const moves = ['U', 'D', 'L', 'R', 'F', 'B'];
-    const endings = size === 'small' ? ['', "'", '2'] : ['', "'", '2', 'w', "w'", 'w2'];
-    const n = moves.length;
-    const m = endings.length;
-    const scramble: string[] = [];
-    let count = 0;
-    while (count < length) {
-      const randMove = moves[this.randomNum(n)];
-      if (
-        (scramble[count - 1] &&
-          (scramble[count - 1].startsWith(randMove) ||
-            (size !== 'large' ? false : scramble[count - 1].startsWith('3' + randMove)))) ||
-        this.active.has(randMove)
-      )
-        continue;
-      this.active.add(randMove);
-      const num = this.randomNum(m);
-      scramble.push(
-        (size === 'large' && num >= 3 && Math.random() < 0.5 ? '3' : '') + randMove + endings[num]
-      );
-      this.removeIntersections(randMove);
-      count++;
-    }
-    return scramble;
-  }
-
+  // Generate a scramble for a given puzzle type
   public generateScramble(type: PuzzleType): string[] {
     switch (type) {
       case PuzzleType['2x2']:
@@ -87,10 +65,40 @@ class ScrambleGenerator {
     }
   }
 
+  // Get the list of available puzzle types
   public getScrambleTypes() {
     return Object.keys(PuzzleType).map((key) => PuzzleType[key]);
   }
 
+  // Generate a standard scramble for a given size and length
+  private standardScramble(size: string, length: number): string[] {
+    const moves = ['U', 'D', 'L', 'R', 'F', 'B'];
+    const endings = size === 'small' ? ['', "'", '2'] : ['', "'", '2', 'w', "w'", 'w2'];
+    const n = moves.length;
+    const m = endings.length;
+    const scramble: string[] = [];
+    let count = 0;
+    while (count < length) {
+      const randMove = moves[this.randomNum(n)];
+      if (
+        (scramble[count - 1] &&
+          (scramble[count - 1].startsWith(randMove) ||
+            (size !== 'large' ? false : scramble[count - 1].startsWith('3' + randMove)))) ||
+        this.active.has(randMove)
+      )
+        continue;
+      this.active.add(randMove);
+      const num = this.randomNum(m);
+      scramble.push(
+        (size === 'large' && num >= 3 && Math.random() < 0.5 ? '3' : '') + randMove + endings[num]
+      );
+      this.removeIntersections(randMove);
+      count++;
+    }
+    return scramble;
+  }
+
+  //generate scramble for pyraminx
   private pyraminxScramble(): string[] {
     const moves = ['U', 'L', 'R', 'B', 'u', 'l', 'r', 'b'];
     const endings = ['', "'", '2'];
@@ -113,6 +121,7 @@ class ScrambleGenerator {
     return scramble;
   }
 
+  //generate scramble for megaminx
   private megaminxScramble(): string[] {
     const moves = ['U', 'R', 'D'];
     const active = {
@@ -143,6 +152,7 @@ class ScrambleGenerator {
     return scramble;
   }
 
+  //generate scramble for skewb
   private skewbScramble(): string[] {
     const moves = ['U', 'L', 'R', 'B'];
     const n = moves.length;
@@ -157,6 +167,7 @@ class ScrambleGenerator {
     return scramble;
   }
 
+  //generate scramble for square1
   private square1Scramble(): string[] {
     const moves = ['U', 'D', 'R', 'L', 'u', 'd', 'r', 'l', 'M', 'E', 'S'];
     const n = moves.length;
@@ -171,6 +182,7 @@ class ScrambleGenerator {
     return scramble;
   }
 
+  //generate scramble for clock
   private clockScramble(): string[] {
     const pins = ['UR', 'DR', 'DL', 'UL'];
     const backMoves = ['U', 'R', 'D', 'L', 'ALL'];
@@ -202,6 +214,8 @@ class ScrambleGenerator {
   }
 }
 
+// Create a new instance of the ScrambleGenerator class
 const scrambler = new ScrambleGenerator();
 
+// Export the ScrambleGenerator class and the PuzzleType enum
 export { scrambler, PuzzleType };
